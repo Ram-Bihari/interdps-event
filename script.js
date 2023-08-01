e4c83cc5f613cce9a7501f2742f792d202d9ecdf
Siddharth Gautam <sidtheprocuber011@gmail.com>
2023-08-01 23:55:17 +0530

    responsive navbar

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-print('succesfully logged in!')
+const navbarBtn = document.querySelector('.toggleBtn')
+const toggleButtonIcon = document.querySelector('.toggleBtn i')
+const dropDownMenu = document.querySelector('.dropdown_menu')
+
+navbarBtn.onclick = function () {
+    dropDownMenu.classList.toggle('open')
+}
