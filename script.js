0352c3d88de4d097f2ca7959f334cc9d48750f5a
Siddharth Gautam <77712603+Ram-Bihari@users.noreply.github.com>
2023-08-08 08:06:19 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const navbarBtn = document.querySelector('.toggleBtn')
-const toggleButtonIcon = document.querySelector('.toggleBtn i')
-const dropDownMenu = document.querySelector('.dropdown_menu')
-
-navbarBtn.onclick = function () {
-    dropDownMenu.classList.toggle('open')
-}
